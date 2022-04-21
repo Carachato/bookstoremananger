3f43fce7e3cbd22905d1cc2d7f5ac4d250d3d30c
CaraChato <carachatoqru@hotmail.com>
2022-04-21 00:03:23 -0300

    primeiro commit

---
+++ src/main/java/com/carachato/bookstoremananger/bookstoremanangerApplication.java
@@ -0,0 +1,13 @@
+package com.carachato.bookstoremananger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class bookstoremanangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(bookstoremanangerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carachato/bookstoremananger/bookstoremanangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carachato.bookstoremananger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class bookstoremanangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
